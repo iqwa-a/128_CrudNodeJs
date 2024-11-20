8df9597ad329e34f9da4135fd14f057e4f6957c1
BlackCat <144578910+iqwa-a@users.noreply.github.com>
2024-11-20 20:59:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
 const express = require('express');
-const app = express();
 const todoRoutes = require('./routes/tododb.js');
+const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
+const expressLayout = require('express-ejs-layouts')
+
+const db = require('./database/db.js')
+
+// pertemuan 7 session dan bycrypt
+const session = require('express-session');
+const authRoutes = require('./routes/authRoutes');
+const { isAuthenticated } = require('./middlewares/middleware.js');
 
 
+app.use(express.static('public'));
+app.use(expressLayout);
 
 app.use(express.json());
 
+
+// Konfigurasi express-session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }  //Set ke true jika menggunakan HTTPS
+}));
+
+
 app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/',isAuthenticated, (req, res) => {
+    res.render('index', {
+        layout: 'layouts/main-layouts.ejs'
+    });
 });
 
-app.get('/contact', (req, res) => {
-    res.render('contact');
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', authRoutes);
+
+app.get('/contact',isAuthenticated,(req, res) => {
+    res.render('contact', {
+        layout: 'layouts/main-layouts.ejs'
+    });
+        
+});
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layouts.ejs',
+            todos: todos
+        });
+    });
 });
 
 app.use((req, res) => {
@@ -25,3 +65,4 @@ app.use((req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
+
